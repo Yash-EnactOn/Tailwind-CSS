61534e4bce962cd0409c7260086891851e1d65b1
Yash Tibadiya <ymtibadiya@enacton.com>
2025-01-03 14:55:23 +0530

    learn tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,60 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      {/* <h1 className="text-3xl bg-green-600 text-white">Hello world!</h1>
+        <h2 className="text-3xl text-yellow-600">Hello world</h2>
+        <p className="border-4 border-red-600">Lorem ipsum dolor sit amet.</p>
+        <p className="text-vuejs-932 text-2xl">vuejs color</p>
+        <p className="text-xs sm:text-base md:text-lg lg:text-3xl">
+          Tailwind CSS
+        </p>
+        <div>
+          <p className="mb-6">Tailwind CSS 1</p>
+          <p className="bg-[#bada55]">Tailwind CSS 2</p>
+        </div>
+        <div className="mt-16 py-4">
+          <h1 className="text-3xl ">Title 1</h1>
+          <h2 className="text-2xl">Title 2</h2>
+          <h3 className="text-xl">Title 3</h3>
+          <p className="text-base underline decoration-red-500 decoration-wavy decoration-1 underline-offset-4 leading-loose capitalize">a regular paragraph</p>
+          <p className="text-sm uppercase">A Description paragraph</p>
+          <p className="text-xs italic note">A little note</p>
+        </div> */}
+
+      {/* <div style={{ width: 300, height: 300 }} className="bg-gray-600">
+          <p className="truncate">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Voluptatibus, rerum corrupti, impedit velit incidunt perferendis
+            laboriosam ratione voluptas porro neque distinctio asperiores fuga.
+          </p>
+        </div> */}
+
+      {/* <div className="whitespace-nowrap">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Voluptatibus, rerum corrupti, impedit velit incidunt perferendis
+            laboriosam ratione voluptas porro neque distinctio asperiores fuga. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis doloremque eum placeat odio quaerat quam, tempore reprehenderit, temporibus quisquam laboriosam minus accusantium fugit repellat consectetur veniam tenetur beatae exercitationem amet?
+          </p>
+        </div> */}
+
+      {/* <div>
+        <div className="m-4 p-4 font-bold text-white bg-red-500">
+          Hello world!
+        </div>
+        <div className="-mt-6">Hello</div>
+      </div> */}
+
+      {/* <div>
+        <div className="w-16 h-16 bg-red-500">Sidebar</div>
+        <div className="w-16 h-16 bg-green-500">Main content</div>
+      </div> */}
+      
+      <div className="flex min-h-screen space-x-3">
+        <div className="w-1/5 bg-red-500">Sidebar</div>
+        <div className="w-4/5 bg-green-500">Main content</div>
+      </div>
+
+      <div></div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,39 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    // spacing: {
+    //   '1': '8px',
+    //   '2': '12px',
+    //   '3': '16px',
+    //   '4': '24px',
+    //   '5': '32px',
+    //   '6': '48px',
+    //   '7': '64px',
+    //   '8': '96px',
+    //   '9': '128px',
+    //   '10': '192px',
+    //   '11': '256px',
+    //   '12': '384px',
+    //   '13': '512px',
+    // },
     extend: {
+      fontSize: {
+        xs: "12px",
+        sm: "15px",
+        base: "18px",
+        lg: "20px",
+      },
       colors: {
+        vuejs: {
+          100: "#49e659",
+          932: "#41b883",
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
