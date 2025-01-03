c4f892816d9f61ccfb579554e503b1d0ccc5b619
Yash Tibadiya <ymtibadiya@enacton.com>
2025-01-03 15:17:22 +0530

    flex wrap

--- app/page.tsx
+++ app/page.tsx
@@ -74,11 +74,24 @@ export default function Home() {
         </div>
       </div> */}
       
-      <div className="parent">
+      {/* <div className="parent">
         <div>Header</div>
         <div className="flex flex-col md:flex-row">
           <div className="md:w-32 bg-slate-400">Sidebar</div>
           <div className="w-full bg-slate-500">Main content</div>
+        </div>
+      </div> */}
+      
+      <div className="parent">
+        <div>Header</div>
+        <div className="flex flex-col md:flex-row">
+           <div className="flex w-full flex-wrap">
+             <div className="w-32 grow">01</div>
+             <div className="w-32 grow">02</div>
+             <div className="w-32 grow">03</div>
+             <div className="w-32 grow">04</div>
+             <div className="w-32 grow">05</div>
+           </div>
         </div>
       </div>
     </div>
